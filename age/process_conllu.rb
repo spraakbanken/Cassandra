a9c8d346bd27ffde9d0e8e7abf4b57250ef03a00
Aleksandrs Berdicevskis <aleksandrs.berdicevskis@gu.se>
2023-03-08 10:42:36 +0100

    Update process_conllu.rb

--- age/process_conllu.rb
+++ age/process_conllu.rb
@@ -1,7 +1,10 @@
 f = File.open("C:\\Sasha\\D\\DGU\\Repos\\Cassandra\\age\\all_age_1000_merged.tsv","r:utf-8")
 agehash = {}
 threshold = 10000
-age_threshold = 18
+firstlasthash = Hash.new{|hash,key| hash[key] = [100,0]}
+age_threshold = 0
+
+
 
 f.each_line.with_index do |line,index|
     line1 = line.strip.split("\t")
@@ -10,11 +13,8 @@ f.each_line.with_index do |line,index|
             agehash[line1[0]] = line1[2].to_i
         else
             break
-
         end
-
     end
-    
 end
 
 
@@ -28,7 +28,11 @@ current_user = nil
 PATHC = "C:\\Sasha\\D\\DGU\\CassandraMy\\SMCorpora\\"
 #PATHC = "C:\\Sasha\\D\\CassandraTemp\\SMCorpora\\"
 f = File.open("#{PATHC}familjeliv-kansliga_sentence.conllu","r:utf-8")
-o = File.open("#{PATHC}familjeliv-kansliga_sentence_age#{threshold}_#{age_threshold}_inverted.conllu","w:utf-8")
+
+###o = File.open("#{PATHC}familjeliv-kansliga_sentence_age#{threshold}_#{age_threshold}_inverted.conllu","w:utf-8")
+o2 = File.open("#{PATHC}familjeliv-kansliga_start_end.tsv","w:utf-8")
+o2.puts "username\tstartage\tendage"
+
 authorhash = {}
 array = []
 tokens = 0
@@ -36,14 +40,20 @@ f.each_line do |line|
     line1 = line.strip
     #STDERR.puts line1
     if line1 == ""
-        
-        
         if !agehash[current_user].nil?
             current_age = current_year - agehash[current_user]
+
+            if current_age < firstlasthash[current_user][0]
+                firstlasthash[current_user][0] = current_age
+            end
+            if current_age > firstlasthash[current_user][1]
+                firstlasthash[current_user][1] = current_age
+            end
+
             #if current_age >=  age_threshold
             if current_age < age_threshold
                 authorhash[current_user] = true
-                o.puts "# age = #{current_age}"
+                ###o.puts "# age = #{current_age}"
                 if current_age < 20
                     agebin = "<20"
                 elsif current_age < 30
@@ -57,10 +67,10 @@ f.each_line do |line|
                 else
                     agebin = ">60"
                 end
-                o.puts "# agebin = #{agebin}"
+                ###o.puts "# agebin = #{agebin}"
 
-                o.puts array
-                o.puts ""
+                ###o.puts array
+                ###o.puts ""
             end
         end
         array = []
@@ -78,5 +88,11 @@ f.each_line do |line|
     end
 
 end
+
+firstlasthash.each_pair do |user,years|
+    o2.puts "#{user}\t#{years[0]}\t#{years[1]}"
+end
+
+__END__
 STDERR.puts authorhash.keys.length
 STDERR.puts tokens
