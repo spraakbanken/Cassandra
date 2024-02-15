27169a04499893e0647ef57f66200397eabbcac1
Aleksandrs Berdicevskis <alexberd@gmail.com>
2024-02-15 11:22:02 +0100

    more infravis

--- wrapper_infravis.rb
+++ wrapper_infravis.rb
@@ -1,4 +1,4 @@
-corpora = ["flashback", "dator", "droger", "ekonomi", "fordon", "hem", "kultur", "livsstil", "mat", "ovrigt", "politik", "samhalle", "sex", "sport", "vetenskap"]
+corpora = ["kultur", "livsstil", "mat", "ovrigt", "politik", "samhalle", "sex", "sport", "vetenskap"]
 corpora.each do |corpus|
     STDERR.puts corpus
     system "ruby query_conllu.rb hbt(q) 0 #{corpus}"
