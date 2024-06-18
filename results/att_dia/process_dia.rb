a1f690c62e2350fc3d1e60e21efef10714ad150b
Aleksandrs Berdicevskis <aleksandrs.berdicevskis@gu.se>
2024-06-18 13:39:16 +0200

    Create process_dia.rb

---
+++ results/att_dia/process_dia.rb
@@ -0,0 +1,35 @@
+verblist = ["försöka", "fortsätta", "glömma", "komma", "slippa", "sluta", "vägra", "behaga", "lova", "planera", "riskera"]
+total_threshold = 0
+
+threshold_per_year = 4
+threshold_years = 4
+
+verblist.each do |verb|
+    authoryear_v2 = Hash.new{|hash, key| hash[key] = Hash.new(0.0)}
+    authoryear_total = Hash.new{|hash, key| hash[key] = Hash.new(0.0)}
+    
+    f = File.open("familjeliv_#{verb}_t#{total_threshold}.tsv","r:utf-8")
+    f.each_line.with_index do |line,index|
+        if index > 0
+            line2 = line.strip.split("\t")
+            year = line2[0].to_i
+            author = line2[1]
+            total = line2[3].to_f
+            v2rel = line2[-1].to_f
+            if total >= threshold_per_year
+                authoryear_v2[author][year] = v2rel
+                authoryear_total[author][year] = total
+            end
+        end
+    end
+    f.close
+    authoryear_v2.each_pair do |author,authorhash|
+        if authorhash.keys.length >= threshold_years
+            o = File.open("#{verb}_#{author}_#{threshold_per_year}_#{threshold_years}.tsv"
+            for year in 2003..2022 do
+                o.puts "#{year}\t#{authorhash[year].to_f}\t#{authoryear_total[author][year].to_f}"
+            end
+        end
+    end
+    o.close
+end
