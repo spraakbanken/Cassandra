afb135ce3acd93938d64646ac66a8f4d1788f9b1
Aleksandrs Berdicevskis <aleksandrs.berdicevskis@gu.se>
2024-06-18 15:12:44 +0200

    att dia data

--- results/att_dia/process_dia.rb
+++ results/att_dia/process_dia.rb
@@ -16,9 +16,12 @@ verblist.each do |verb|
             line2 = line.strip.split("\t")
             year = line2[0].to_i
             author = line2[1]
-            total = line2[3].to_f
+            total = line2[4].to_f
+            #STDERR.puts year, author, total
+            #sleep(1)
             v2rel = line2[-1].to_f
             if total >= threshold_per_year
+                #STDERR.puts "CHECK!"
                 authoryear_v2[author][year] = v2rel
                 authoryear_total[author][year] = total
             end
@@ -27,12 +30,14 @@ verblist.each do |verb|
     f.close
     STDERR.puts "Writing"
     authoryear_v2.each_pair do |author,authorhash|
+        #STDERR.puts author,authorhash.keys.length
         if authorhash.keys.length >= threshold_years
-            o = File.open("individuals\\#{verb}_#{author}_#{threshold_per_year}_#{threshold_years}.tsv"
+            o = File.open("individuals\\#{verb}_#{author}_#{threshold_per_year}_#{threshold_years}.tsv", "w:utf-8")
             for year in 2003..2022 do
                 o.puts "#{year}\t#{authorhash[year].to_f}\t#{authoryear_total[author][year].to_f}"
             end
+            o.close
         end
     end
-    o.close
+    
 end
