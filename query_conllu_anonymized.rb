71d1fd9da5dcbf161bf8c8c4e3461f0d6bfea1f4
Aleksandrs Berdicevskis <aleksandrs.berdicevskis@gu.se>
2024-09-11 12:27:04 +0200

    Update query_conllu_anonymized.rb

--- query_conllu_anonymized.rb
+++ query_conllu_anonymized.rb
@@ -1,4 +1,4 @@
-
+bysubforum = true
 
 variable = ARGV[0]
 if variable.to_s == ""
@@ -39,7 +39,7 @@ an_file.close
 
 
 #subforums = ["resor"]
-subforums = ["resor"]
+subforums = ["resor","mat"]
 #subforums = ["dator", "droger", "ekonomi", "flashback", "fordon", "hem", "kultur", "livsstil", "mat", "ovrigt", "politik", "resor", "samhalle", "sex", "sport", "vetenskap"]
 
 #subforums = ["adoption","allmanna-ekonomi","allmanna-familjeliv","allmanna-fritid","allmanna-husdjur","allmanna-hushem","allmanna-kropp","allmanna-noje","allmanna-samhalle","allmanna-sandladan","anglarum","foralder","gravid","kansliga","medlem-allmanna","medlem-foraldrar","medlem-planerarbarn","medlem-vantarbarn","pappagrupp","planerarbarn","sexsamlevnad","svartattfabarn","expert"]
@@ -114,9 +114,9 @@ subforums.each do |subforum|
             elsif line1.include?("# username")
                 current_username2 = line1.split(" = ")[1]
                 current_username = userhash[current_username2]
-                if current_username.nil? or current_username == ""
-                    missing_usernames[current_username2] = true
-                end
+                #if current_username.nil? or current_username == ""
+                    #missing_usernames[current_username2] = true
+                #end
             elsif line1.include?("# post_date")
                 current_year = line1.split(" = ")[1].split("-")[0].to_i
             
@@ -175,67 +175,131 @@ subforums.each do |subforum|
             end
         end
     end
-
-
-end
-
-corpus2 = "#{corpus}-#{subforums.join(",")}"
-
-if with_age
-    o = File.open("results\\familjeliv_#{variable}_t#{total_threshold}_#{year_of_interest}.tsv","w:utf-8")
-    o.puts "period\tusername\tage\tagebin\ttotal\tv1abs\tv2abs\tv1rel\tv2rel"
-else
-    o = File.open("results\\#{corpus2}_#{variable}_t#{total_threshold}.tsv","w:utf-8")
-    o.puts "period\tusername\ttotal\tv1abs\tv2abs\tv1rel\tv2rel"
-end
-
-
-
-nprolific = 0.0
-sum_v2rel = 0.0
-
-if with_age
-    authorhash.each_pair do |key,value|
-        year = key[3]
-        username = key[0]
-        age = key[1]
-        #bin = key[2]
-        total = value["total"]
-        if total >= total_threshold
-            nprolific += 1
-            v1abs = value["v1"]
-            v2abs = value["v2"]
-            v3abs = value["v3"]
-            v1rel = v1abs/total
-            v2rel = v2abs/total
-            sum_v2rel += v2rel
-            v3rel = v3abs/total
-            #o.puts "#{year}\t#{username}\t#{age}\t#{bin}\t#{total}\t#{v1abs}\t#{v2abs}\t#{v3abs}\t#{v1rel}\t#{v2rel}\t#{v3rel}"
-            #o.puts "#{year}\t#{username}\t#{age}\t#{bin}\t#{total}\t#{v1abs}\t#{v2abs}\t#{v1rel}\t#{v2rel}"
-            o.puts "#{year}\t#{username}\t#{age}\t\t#{total}\t#{v1abs}\t#{v2abs}\t#{v1rel}\t#{v2rel}"
+    
+    if bysubforum
+        
+        corpus2 = "#{corpus}-#{subforum}"
+        
+        if with_age
+            o = File.open("results\\familjeliv_#{variable}_t#{total_threshold}_#{year_of_interest}.tsv","w:utf-8")
+            o.puts "period\tusername\tage\tagebin\ttotal\tv1abs\tv2abs\tv1rel\tv2rel"
+        else
+            o = File.open("results\\#{corpus2}_#{variable}_t#{total_threshold}.tsv","w:utf-8")
+            o.puts "period\tusername\ttotal\tv1abs\tv2abs\tv1rel\tv2rel"
         end
-    
-    end
-    o.close
-    
-    STDERR.puts "#{variable} #prolific speakers: #{nprolific} Average v2rel: #{sum_v2rel/nprolific}"
-    if nprolific >= 10 and sum_v2rel/nprolific >= 0.01 and  sum_v2rel/nprolific <= 0.90
-        File.rename("results\\ss30_#{year_of_interest}\\familjeliv_#{variable}_t#{total_threshold}_#{year_of_interest}.tsv", "results\\ss30_#{year_of_interest}\\familjeliv_#{variable}_t#{total_threshold}_#{year_of_interest}_a.tsv")
-    end
-else
-    authorhash_new.each_pair do |year,value|
-        value.each_pair do |username,varhash|
-            total = varhash["total"]
-            v1abs = varhash["v1"]
-            v2abs = varhash["v2"]
-            v1rel = v1abs/total
-            v2rel = v2abs/total
-            o.puts "#{year}\t#{username}\t#{total}\t#{v1abs}\t#{v2abs}\t#{v1rel}\t#{v2rel}"
+        
+        
+        
+        nprolific = 0.0
+        sum_v2rel = 0.0
+        
+        if with_age
+            authorhash.each_pair do |key,value|
+                year = key[3]
+                username = key[0]
+                age = key[1]
+                #bin = key[2]
+                total = value["total"]
+                if total >= total_threshold
+                    nprolific += 1
+                    v1abs = value["v1"]
+                    v2abs = value["v2"]
+                    v3abs = value["v3"]
+                    v1rel = v1abs/total
+                    v2rel = v2abs/total
+                    sum_v2rel += v2rel
+                    v3rel = v3abs/total
+                    #o.puts "#{year}\t#{username}\t#{age}\t#{bin}\t#{total}\t#{v1abs}\t#{v2abs}\t#{v3abs}\t#{v1rel}\t#{v2rel}\t#{v3rel}"
+                    #o.puts "#{year}\t#{username}\t#{age}\t#{bin}\t#{total}\t#{v1abs}\t#{v2abs}\t#{v1rel}\t#{v2rel}"
+                    o.puts "#{year}\t#{username}\t#{age}\t\t#{total}\t#{v1abs}\t#{v2abs}\t#{v1rel}\t#{v2rel}"
+                end
+            
+            end
+            o.close
+            
+            STDERR.puts "#{variable} #prolific speakers: #{nprolific} Average v2rel: #{sum_v2rel/nprolific}"
+            if nprolific >= 10 and sum_v2rel/nprolific >= 0.01 and  sum_v2rel/nprolific <= 0.90
+                File.rename("results\\ss30_#{year_of_interest}\\familjeliv_#{variable}_t#{total_threshold}_#{year_of_interest}.tsv", "results\\ss30_#{year_of_interest}\\familjeliv_#{variable}_t#{total_threshold}_#{year_of_interest}_a.tsv")
+            end
+        else
+            authorhash_new.each_pair do |year,value|
+                value.each_pair do |username,varhash|
+                    total = varhash["total"]
+                    v1abs = varhash["v1"]
+                    v2abs = varhash["v2"]
+                    v1rel = v1abs/total
+                    v2rel = v2abs/total
+                    o.puts "#{year}\t#{username}\t#{total}\t#{v1abs}\t#{v2abs}\t#{v1rel}\t#{v2rel}"
+                end
+            
+            end
+            o.close
+            
         end
-    
+
     end
-    o.close
-    
+
+ 
 end
 
-STDOUT.puts missing_usernames.keys
+if !bysubforum
+    corpus2 = "#{corpus}-#{subforums.join(",")}"
+    
+    if with_age
+        o = File.open("results\\familjeliv_#{variable}_t#{total_threshold}_#{year_of_interest}.tsv","w:utf-8")
+        o.puts "period\tusername\tage\tagebin\ttotal\tv1abs\tv2abs\tv1rel\tv2rel"
+    else
+        o = File.open("results\\#{corpus2}_#{variable}_t#{total_threshold}.tsv","w:utf-8")
+        o.puts "period\tusername\ttotal\tv1abs\tv2abs\tv1rel\tv2rel"
+    end
+    
+    
+    
+    nprolific = 0.0
+    sum_v2rel = 0.0
+    
+    if with_age
+        authorhash.each_pair do |key,value|
+            year = key[3]
+            username = key[0]
+            age = key[1]
+            #bin = key[2]
+            total = value["total"]
+            if total >= total_threshold
+                nprolific += 1
+                v1abs = value["v1"]
+                v2abs = value["v2"]
+                v3abs = value["v3"]
+                v1rel = v1abs/total
+                v2rel = v2abs/total
+                sum_v2rel += v2rel
+                v3rel = v3abs/total
+                #o.puts "#{year}\t#{username}\t#{age}\t#{bin}\t#{total}\t#{v1abs}\t#{v2abs}\t#{v3abs}\t#{v1rel}\t#{v2rel}\t#{v3rel}"
+                #o.puts "#{year}\t#{username}\t#{age}\t#{bin}\t#{total}\t#{v1abs}\t#{v2abs}\t#{v1rel}\t#{v2rel}"
+                o.puts "#{year}\t#{username}\t#{age}\t\t#{total}\t#{v1abs}\t#{v2abs}\t#{v1rel}\t#{v2rel}"
+            end
+        
+        end
+        o.close
+        
+        STDERR.puts "#{variable} #prolific speakers: #{nprolific} Average v2rel: #{sum_v2rel/nprolific}"
+        if nprolific >= 10 and sum_v2rel/nprolific >= 0.01 and  sum_v2rel/nprolific <= 0.90
+            File.rename("results\\ss30_#{year_of_interest}\\familjeliv_#{variable}_t#{total_threshold}_#{year_of_interest}.tsv", "results\\ss30_#{year_of_interest}\\familjeliv_#{variable}_t#{total_threshold}_#{year_of_interest}_a.tsv")
+        end
+    else
+        authorhash_new.each_pair do |year,value|
+            value.each_pair do |username,varhash|
+                total = varhash["total"]
+                v1abs = varhash["v1"]
+                v2abs = varhash["v2"]
+                v1rel = v1abs/total
+                v2rel = v2abs/total
+                o.puts "#{year}\t#{username}\t#{total}\t#{v1abs}\t#{v2abs}\t#{v1rel}\t#{v2rel}"
+            end
+        
+        end
+        o.close
+        
+    end
+end
+#STDOUT.puts missing_usernames.keys
