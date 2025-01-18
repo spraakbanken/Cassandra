0a1779b0ca7d20113f98cfea5979b013fe719953
Aleksandrs Berdicevskis <alexberd@gmail.com>
2025-01-18 21:01:21 +0100

    Update query_conllu_ss30syn_for_dia_opt_anonymized_incl0users.rb

--- query_conllu_ss30syn_for_dia_opt_anonymized_incl0users.rb
+++ query_conllu_ss30syn_for_dia_opt_anonymized_incl0users.rb
@@ -6,6 +6,7 @@ require_relative "queries\\query_tools.rb"
 #subforums = ["adoption"]
 subforums = ["adoption","allmanna-ekonomi","allmanna-familjeliv","allmanna-fritid","allmanna-husdjur","allmanna-hushem","allmanna-kropp","allmanna-noje","allmanna-samhalle","allmanna-sandladan","anglarum","foralder","gravid","kansliga","medlem-allmanna","medlem-foraldrar","medlem-planerarbarn","medlem-vantarbarn","pappagrupp","planerarbarn","sexsamlevnad","svartattfabarn","expert"]
 an_path = "C:\\Sasha\\D\\DGU\\CassandraMy\\SMCorpora\\"
+corpus = "familjeliv"
 an_file = File.open("#{an_path}#{corpus}_usernames.tsv","r:utf-8")
 
 "Reading in userhash..."
@@ -21,8 +22,14 @@ an_file.each_line.with_index do |line,index|
 end
 an_file.close
 
-#PATH = "C:\\Sasha\\D\\DGU\\CassandraMy\\SMCorpora\\familjeliv-age\\"
-PATH = "D:\\D\\DGU\\CassandraMy\\SMCorpora\\familjeliv-age\\"
+with_age = false
+if with_age
+    PATH = "C:\\Sasha\\D\\DGU\\CassandraMy\\SMCorpora\\familjeliv-age\\"
+    #PATH = "D:\\D\\DGU\\CassandraMy\\SMCorpora\\familjeliv-age\\"
+else
+    PATH = "C:\\Sasha\\D\\DGU\\CassandraMy\\SMCorpora\\familjeliv-age\\"
+    #PATH = "D:\\D\\DGU\\CassandraMy\\SMCorpora\\familjeliv-age\\"
+end
 token_threshold = 10000
 firstage = 18
 total_threshold = 0
@@ -69,7 +76,11 @@ verblist = ["försöka", "fortsätta", "glömma", "komma", "slippa", "sluta", "v
     
 subforums.each do |subforum|
     STDERR.puts "#{subforum}"
-    f = File.open("#{PATH}familjeliv-#{subforum}_sentence_age#{token_threshold}_#{firstage}.conllu","r:utf-8")
+    if with_age
+        f = File.open("#{PATH}familjeliv-#{subforum}_sentence_age#{token_threshold}_#{firstage}.conllu","r:utf-8")
+    else
+        f = File.open("#{PATH}#{corpus]}-#{subforum}_sentence.conllu","r:utf-8")
+    end
     
     current_age = ""
     current_agebin = ""
@@ -117,7 +128,9 @@ subforums.each do |subforum|
             elsif line1.include?("# username")
                 current_username = line1.split(" = ")[1]
                 current_username = userhash[current_username]
-                authorhash[current_year][verb_of_interest][[current_username,yob]]["total"] = authorhash[current_year][verb_of_interest][[current_username,yob]]["total"]
+                verblist.each do |verb_of_interest|
+                    authorhash[current_year][verb_of_interest][[current_username,yob]]["total"] = authorhash[current_year][verb_of_interest][[current_username,yob]]["total"]
+                end
             elsif line1.include?("# post_date")
                 current_year = line1.split(" = ")[1].split("-")[0].to_i
             
@@ -180,6 +193,13 @@ totaltotal_all = 0.0
 v2abstotal = 0.0
 v2abstotal_all = 0.0
 nspeakers = authorhash.keys.length
+
+if with_age
+    outdir = "att_dia2"
+else
+    outdir = "att_dia2_all"
+end
+
 for year in 2003..2022 do 
     #oo = File.open("results\\att_dia\\summary_#{year}_t#{total_threshold}.tsv","w:utf-8")
     #oo.puts "verb\tnprolific_speakers\ttotal_prolific\tmicroave_v2rel_prolific\tnspeakers\ttotal\tmicroave_v2rel" 
@@ -187,9 +207,13 @@ for year in 2003..2022 do
 
     yearhash = authorhash[year]
     yearhash.each_pair do |verb,verbhash|
-        o = File.open("results\\att_dia\\familjeliv_#{verb}_t#{total_threshold}.tsv","a:utf-8")
+        o = File.open("results\\#{outdir}\\familjeliv_#{verb}_t#{total_threshold}.tsv","a:utf-8")
         if year == 2003
-            o.puts "period\tusername\tyob\tagebin\ttotal\tv1abs\tv2abs\tv1rel\tv2rel"
+            if with_age
+                o.puts "period\tusername\tyob\tagebin\ttotal\tv1abs\tv2abs\tv1rel\tv2rel"
+            else
+                o.puts "period\tusername\ttotal\tv1abs\tv2abs\tv1rel\tv2rel"
+            end
         end
 
         verbhash.each_pair do |key,value|
@@ -203,7 +227,11 @@ for year in 2003..2022 do
                 v1abs = value["v1"]
                 v1rel = div_by_zero_marked(v1abs,total)
                 v2rel = div_by_zero_marked(v2abs,total)
-                o.puts "#{year}\t#{username}\t#{yob}\t\t#{total}\t#{v1abs}\t#{v2abs}\t#{v1rel}\t#{v2rel}"
+                if with_age
+                    o.puts "#{year}\t#{username}\t#{yob}\t\t#{total}\t#{v1abs}\t#{v2abs}\t#{v1rel}\t#{v2rel}"
+                else
+                    o.puts "#{year}\t#{username}\t#{total}\t#{v1abs}\t#{v2abs}\t#{v1rel}\t#{v2rel}"
+                end
                 #totaltotal += total
                 #v2abstotal += v2abs
             end
