678f578d7bd8a37b4612cdf25f345a7fc5cdd93f
Aleksandrs Berdicevskis <alexberd@gmail.com>
2026-02-05 18:20:05 +0100

    jagged

--- process_att90_2026.rb
+++ process_att90_2026.rb
@@ -5,14 +5,14 @@ files = Dir.children(path)
 verbs = Hash.new{|hash,key| hash[key]=Hash.new}
 verb_centered = Hash.new{|hash,key| hash[key]=Hash.new}
 #verblist = ["komma"]
-verblist = ["besluta","hota","planera","lova","tendera","riskera","avse","fortsätta","komma","förmå","glömma","behaga","vägra","anse","sluta","idas","slippa","försöka","låtsas","lyckas","hinna","börja","orka","våga","behöva","bruka","råka","torde"]
+verblist = ["besluta","hota","planera","lova","tendera","riskera","avse","fortsätta","komma","förmå","glömma","behaga","vägra","anse","sluta","idas","slippa","försöka","låtsas","lyckas","hinna","börja","orka","våga","behöva","bruka","råka","torde","ämna","förefalla"]
 verbs_total = Hash.new(0)
 
-#"ämna","förefalla",
+#,
 @startyear = 2004
 @lastyear = 2022
 
-threshold = 100
+threshold = 1000
 
 
 
@@ -32,7 +32,7 @@ def jaggedness(verb,years,values)
     end
     direct = Math.sqrt((years[@lastyear] - years[@startyear]).abs2 + (values[@lastyear] - values[@startyear]).abs2)
     
-    jagged = (indirect) / direct
+    jagged = div_by_zero(indirect, direct)
     return [jagged,direct]
 end
 
@@ -62,17 +62,20 @@ end
 #STDERR.puts verblist
 #__END__
 STDOUT.puts "verb\tfreq\tjaggedness"
-verbs.each_pair do |verb,yearhash|
+#verbs.each_pair do |verb,yearhash|
+verblist.each do |verb|
+    yearhash = verbs[verb]
     centeredyears_a = center(yearhash.keys)
     #STDERR.puts centeredyears_a
-    centeredvalues_a = center(yearhash.values)
+    centeredvalues_a = smooth(center(yearhash.values),3)
     centeredyears = {}
     centeredvalues = {}
     yearhash.keys.each.with_index do |year,index|
         centeredyears[year] = centeredyears_a[index]
         centeredvalues[year] = centeredvalues_a[index]
     end
-
+    MOVE TO R
+    R.eval "plot(values ~ years, ylim = c(0,1), pch=21, col = 'black', bg='black',type='l')"
     jagged = jaggedness(verb,centeredyears,centeredvalues)
     STDOUT.puts "#{verb}\t#{verbs_total[verb]}\t#{jagged[0].round(9)}"
 end
