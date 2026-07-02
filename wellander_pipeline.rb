af39df85d364e0dd0d0150a9d801987d23648a5a
Aleksandrs Berdicevskis <alexberd@gmail.com>
2026-07-02 11:43:05 +0200

    upd pipeline

--- wellander_pipeline.rb
+++ wellander_pipeline.rb
@@ -8,25 +8,29 @@ corpus = "pressfull"
 timecorpus = "svt-all"
 coverage = 0.5
 
-$verblist.each do |verb|
-    STDERR.puts verb
-    if generate_inf_queries
-        system "ruby w_generate_inf_queries.rb"
-    end
-    
-    if run_inf_queries
-        system "ruby w_run_inf_queries.rb #{corpus}"
-    end
-    
-    if generate_half_queries
-        system "ruby w_generate_half_queries.rb #{corpus} #{coverage}"
-    end
-
-    if run_half_queries
-        system "ruby w_run_half_queries.rb #{timecorpus}"
-    end
-    
-    if compare_queries
-        system "ruby w_compare_queries.rb #{timecorpus} #{coverage}"
-    end
+#$verblist.each do |verb|
+#STDERR.puts verb
+if generate_inf_queries
+    system "ruby w_generate_inf_queries.rb"
 end
+
+if run_inf_queries
+    system "ruby w_run_inf_queries.rb #{corpus}"
+end
+
+if generate_half_queries
+    system "ruby w_generate_half_queries.rb #{corpus} #{coverage}"
+end
+
+if run_half_queries
+    system "ruby w_run_half_queries.rb #{timecorpus}"
+end
+
+if compare_queries
+    system "ruby w_compare_queries.rb #{timecorpus} #{coverage}"
+end
+
+if w_plot_retriever
+    
+end
+#end
